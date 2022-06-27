2456ff4ee06ff7c062599e18908eb41477d45030
dida nurdiansyah <dida.nurdiansyah@gmail.com>
2022-06-28 06:16:33 +0700

    mobile

--- index4.js
+++ index4.js
@@ -4,4 +4,17 @@ $(document).ready((event) => {
   $("#active-call").toggleSwitch();
   $("#active-sms").toggleSwitch();
   $("#active-email").toggleSwitch();
+
+  $("#subscription-01").toggleSwitch();
+  $("#subscription-02").toggleSwitch();
+  $("#subscription-03").toggleSwitch();
+  $("#subscription-04").toggleSwitch();
+  $("#subscription-05").toggleSwitch();
+  $("#subscription-06").toggleSwitch();
+  $("#subscription-07").toggleSwitch();
+  $("#subscription-08").toggleSwitch();
+  $("#subscription-09").toggleSwitch();
+  $("#subscription-10").toggleSwitch();
+  $("#subscription-11").toggleSwitch();
+  $("#subscription-12").toggleSwitch();
 });
