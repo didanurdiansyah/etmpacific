f2d2b21cd368c4a365ad807bc736ced83e668e9b
dida nurdiansyah <dida.nurdiansyah@gmail.com>
2022-06-27 10:45:53 +0700

    new

---
+++ index4.js
@@ -0,0 +1,7 @@
+$(document).ready((event) => {
+  $("#set-email-limit").toggleSwitch();
+  $("#active-et-command").toggleSwitch();
+  $("#active-call").toggleSwitch();
+  $("#active-sms").toggleSwitch();
+  $("#active-email").toggleSwitch();
+});
